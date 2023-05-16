d9acc3aa991d4a7ed654d68eda56875ec7a070a0
Yacov Kopel <kokokopel94@gmail.com>
2023-05-16 14:36:37 -0700

    input options added and styled

--- src/Feed.js
+++ src/Feed.js
@@ -1,6 +1,7 @@
-import { Create } from '@mui/icons-material'
+import { CalendarViewDay, Create, EventNote, Image, Subscriptions } from '@mui/icons-material'
 import React from 'react'
 import './Feed.css'
+import InputOption from './InputOption'
 
 function Feed() {
 
@@ -10,12 +11,23 @@ function Feed() {
 
     <div className='feed'>
         <div className='feed_inputContainer'>
+            
             <div className='feed_input'>
                 <Create/>
                 <form>
                     <input type="text" placeholder="What's on your mind?"/>
                     <button type='submit'>Send</button>
                 </form>
+
+            </div>
+            <div className='feed_inputOptions'>
+                <InputOption Icon={Image}title = "Photo" color="#7085f9"/>
+                <InputOption Icon={Subscriptions}title = "Video" color="#E7A33E"/>
+                <InputOption Icon={EventNote}title = "Event" color="#C0CBCD"/>
+                <InputOption Icon={CalendarViewDay}title = "Write Article" color="#7FC15E"/>
+
+
+
 
             </div>
         </div>
