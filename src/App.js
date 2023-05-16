f37ab0d10c9bd840a7d4e6f8356ae3412f89080e
Yacov Kopel <kokokopel94@gmail.com>
2023-05-16 11:25:36 -0700

    adde header component and installed Material UI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
+import Header from './Header'
 
 function App() {
   return (
     <div className="App">
       LEts build a linked in CLone
+
+    <Header />
+
+    {/* {APPBoDY
+      SIDEBAR
+      FEED
+      WIdgets */}
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Header.css'
+
+
+function Header() {
+
+
+  return (
+    <div className='heder'>
+        this is the Header
+
+<div className='header_left'>
+<img src='' alt=''/>
+<div className='header_search'>
+    <input type='text'/>
+</div>
+</div>
+<div className='header_right'>
+
+
+</div>
+    </div>
+  )
+}
+
+export default Header
