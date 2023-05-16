2f2f7d1d0d7e9e5d2caf1971500e87bf69da475a
Yacov Kopel <kokokopel94@gmail.com>
2023-05-16 13:55:32 -0700

    styled sidebar top and bottom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
 import Header from './Header'
+import Sidebar from './Sidebar';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
 
     <Header />
 <div className='app_body'>
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,36 +1,43 @@
-import { Avatar } from '@mui/material'
-import React from 'react'
+import { Avatar } from "@mui/material";
+import React from "react";
+import "./Sidebar.css";
 
 function Sidebar() {
+  const recentItem = (topic) => (
+    <div className="sidebar_recentItem">
+      <span className="sidebar_hash">#</span>
+      <p>{topic}</p>
+    </div>
+  );
   return (
-    <div className='sidebar'>
-        <div className='sidebar_top'>
-        <img src='' alt=''/>
-        <Avatar className='sidebar_avatar'/>
+    <div className="sidebar">
+      <div className="sidebar_top">
+        <img src="" alt="" />
+        <Avatar className="sidebar_avatar" />
         <h2>Yacov Kopel</h2>
         <h4>yacovkopel@gmail.com</h4>
+      </div>
+
+      <div className="sidebar_stats">
+        <div className="sidebar_stat">
+          <p>Who viewed you</p>
+          <p className="sidebar_statNumber">1,393</p>
         </div>
-
-        <div className='sidebar_stats'>
-
-        <div className='sidebar_stat'>
-            <p>Who viewed you</p>
-            <p className='sidebar_statNumber'>1,393</p>
-
+        <div className="sidebar_stat">
+          <p>Views on post</p>
+          <p className="sidebar_statNumber">1,393</p>
         </div>
-        <div className='sidebar_stat'>
-            <p>Views on post</p>
-            <p className='sidebar_statNumber'>1,393</p>
+      </div>
 
-        </div>
-        </div>
-
-        <div className='sidebar_bottom'>
-            <p>Recent Post</p>
-            </div>
-
-        </div>
-  )
+      <div className="sidebar_bottom">
+        <p>Recent</p>
+        {recentItem("reactjs")}
+        {recentItem("software developer")}
+        {recentItem("programming")}
+        {recentItem("design")}
+      </div>
+    </div>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
