61a7f5de98d249df77c96b4150fd1a491135a8ba
Yacov Kopel <kokokopel94@gmail.com>
2023-05-16 12:44:38 -0700

    created Header with buttons/icons and their css

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Header from './Header'
 function App() {
   return (
     <div className="App">
-      LEts build a linked in CLone
 
     <Header />
 
--- src/Header.js
+++ src/Header.js
@@ -1,21 +1,31 @@
 import React from 'react'
 import './Header.css'
+import { BusinessCenter, Chat, Home, Notifications, Search, SupervisorAccount } from '@mui/icons-material'
+import HeaderOption from './HeaderOption'
 
 
 function Header() {
 
 
   return (
-    <div className='heder'>
-        this is the Header
+    <div className='header'>
+
 
 <div className='header_left'>
-<img src='' alt=''/>
+<img src='/linkedin.png' alt=''/>
 <div className='header_search'>
+    <Search />
     <input type='text'/>
 </div>
 </div>
 <div className='header_right'>
+    <HeaderOption Icon={Home} title="Home"/>
+    <HeaderOption  Icon={SupervisorAccount} title= "My Network" />
+    <HeaderOption Icon={BusinessCenter} title="Jobs"/>
+    <HeaderOption Icon={Chat} title="Home"/>
+    <HeaderOption Icon={Notifications} title="Notification"/>
+    <HeaderOption avatar="https://media.licdn.com/dms/image/D5635AQE9ggjXQLkT9w/profile-framedphoto-shrink_400_400/0/1676571398237?e=1684872000&v=beta&t=4Zh7vQva1hbYHB9EPK8YklC30zU5UqpRm0VY_9rLWwA"/>
+
 
 
 </div>
