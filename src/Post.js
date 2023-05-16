fd31220cc63ec170fee5c1fc722a4a969caa22a7
Yacov Kopel <kokokopel94@gmail.com>
2023-05-16 15:02:21 -0700

    added post buttons

--- src/Post.js
+++ src/Post.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import "./Post.css"
 import { Avatar } from "@mui/material";
+import InputOption from './InputOption';
+import { ChatOutlined, SendOutlined, ShareOutlined, ThumbUpAltOutlined } from '@mui/icons-material';
 
 
 function Post({name, description,message, photoUrl}) {
@@ -15,6 +17,15 @@ function Post({name, description,message, photoUrl}) {
             </div>
             <div className='post_body'>
                 <p>{message}</p>
+            </div>
+            <div className='post_buttons'>
+            <InputOption Icon={ThumbUpAltOutlined} title="Like" color="gray"/>
+            <InputOption Icon={ChatOutlined} title="Comment" color="gray"/>
+            <InputOption Icon={ShareOutlined} title="Share" color="gray"/>
+            <InputOption Icon={SendOutlined} title="Send" color="gray"/>
+
+
+
             </div>
 
 
