ea86b132466b2295e9d221682f13523a48216317
Yacov Kopel <kokokopel94@gmail.com>
2023-05-22 23:14:27 -0700

    widget added

--- src/Widget.js
+++ src/Widget.js
@@ -1,8 +1,30 @@
+import { FiberManualRecord, Info } from '@mui/icons-material'
 import React from 'react'
+import './Widget.css'
 
 function Widget() {
+    const newsArticle= (heading, subtitle) =>(
+    <div className='widgets_article'>
+        <div className='widgets_articleLeft'>
+            <FiberManualRecord/>
+        </div>
+    <div className='widgets_articleRight'>
+        <h4>{heading}</h4>
+        <p>{subtitle}</p>
+    </div>
+    </div>
+    )
   return (
-    <div>Widget</div>
+    <div className='widgets'>
+        <div className='widgets_header'>
+            <h2>
+                LinkedIn News
+            </h2>
+            <Info/>
+
+        </div>
+        {newsArticle("we out here", "we making apps happen")}
+    </div>
   )
 }
 
